249f35556b455985e52ccc1077ea83e440c95eec
Niklas <nmhjort7@gmail.com>
2024-07-14 01:03:51 +0200

    file setup

--- php/library/src/index.php
+++ php/library/src/index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
-    <h1>app2</h1>
+    <script src="/static/custom-script.js"></script>
 </body>
 </html>
